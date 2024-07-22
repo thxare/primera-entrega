f681b698e7746a65b57d98a4764be18ec53abd0e
Thiare <thiareayleen@gmail.com>
2024-07-22 19:06:15 -0400

    updates

--- src/dao/models/cart.model.js
+++ src/dao/models/cart.model.js
@@ -16,6 +16,11 @@ const cartSchema = new mongoose.Schema({
   ],
 });
 
+cartSchema.pre("findOne", function (next) {
+  this.populate("products.product", "_id title price");
+  next();
+});
+
 const CartModel = mongoose.model("carts", cartSchema);
 
 export default CartModel;
